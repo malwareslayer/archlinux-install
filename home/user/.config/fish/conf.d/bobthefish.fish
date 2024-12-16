36aea73c4f173ed5812f8d30797c2c654bc19d47
Dwi Mulia Mokoginta <dwi-mulia-mokoginta@protonmail.com>
2024-12-16 18:11:32 +0700

    Add fish

---
+++ home/user/.config/fish/conf.d/bobthefish.fish
@@ -0,0 +1,15 @@
+set -g theme_display_jobs_verbose yes
+set -g theme_display_user yes
+set -g theme_display_hostname yes
+set -g theme_display_screen yes
+
+set -g theme_title_display_process yes
+set -g theme_title_display_path yes
+set -g theme_title_display_user yes
+
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts yes
+set -g theme_color_scheme dark
+
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_project_dir_length 0
---
+++ home/user/.config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+        # Do Nothing
+end
+
+set -x GPG_TTY (tty)
+
+gpg-connect-agent updatestartuptty /bye > /dev/null
+
+if not test -n "$TMUX"
+        tmux attach || tmux new
+end
