5609b2408164f1ca6d2a096396dd54fedfc74235
Dwi Mulia Mokoginta <dwi-mulia-mokoginta@protonmail.com>
2024-12-18 11:15:57 +0700

    Update bobthefish config

--- home/user/.config/fish/conf.d/bobthefish.fish
+++ home/user/.config/fish/conf.d/bobthefish.fish
@@ -13,9 +13,14 @@ set -g theme_display_node yes
 set -g theme_display_go verbose
 
 set -g theme_display_git yes
+set -g theme_display_git_dirty_verbose no
+set -g theme_display_git_ahead_verbose no
+set -g theme_display_git_stashed_verbose no
+set -g theme_display_git_default_branch no
+set -g theme_use_abbreviated_branch_name no
 
 set -g theme_powerline_fonts yes
-set -g theme_nerd_fonts yes
+set -g theme_nerd_fonts no
 set -g theme_color_scheme dark
 
 set -g fish_prompt_pwd_dir_length 0
