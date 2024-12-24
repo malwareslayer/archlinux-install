3bc829e711839bff61be958232d8040b6b12fd34
Dwi Mulia Mokoginta <dwi-mulia-mokoginta@protonmail.com>
2024-12-24 22:50:35 +0700

    User local binary PATH

--- home/user/.config/fish/config.fish
+++ home/user/.config/fish/config.fish
@@ -1,11 +1,15 @@
 if status is-interactive
-        # Do Nothing
+    # Do Nothing
 end
 
-set -x GPG_TTY (tty)
+if not contains "/home/$USER/.local/bin" $PATH
+	set PATH $PATH /home/malwareslayer/.local/bin
+end
+
+set GPG_TTY (tty)
 
 gpg-connect-agent updatestartuptty /bye > /dev/null
 
 if not test -n "$TMUX"
-        tmux
+    tmux
 end
