ce066b9e7da8eed02ed0504b83fe1442704f17df
Dwi Mulia Mokoginta <dwi-mulia-mokoginta@protonmail.com>
2024-12-24 22:50:48 +0700

    Add some Tailscale status

---
+++ home/user/.config/fish/functions/fish_tailscale.fish
@@ -0,0 +1,12 @@
+function fish_tailscale -d 'Show Status Tailscale'
+    set --local _hostname "$(hostnamectl hostname)"
+    set --local _devices "$(tailscale --socket /run/user/60466/tailscale/tailscaled.sock status | awk '/^\s*$/ {exit} {print}')"
+    set --local _total "$(echo "$_devices" | wc -l)"
+    set --local _offline "$(echo "$_devices" | grep -c 'offline')"
+
+    if echo "$_devices" | grep -q "$_hostname.*offline"
+        echo "Disconnect ($(math $_total - $_offline) Last Online)"
+    else
+        echo "$_total ($(math $_total - $_offline) Online)"
+    end
+end
