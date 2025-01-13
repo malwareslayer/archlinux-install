1e5620e48d0791c3e6cca28562d7fa3433a93e2f
Dwi Mulia Mokoginta <dwi-mulia-mokoginta@protonmail.com>
2025-01-13 19:23:52 +0700

    Normalize & startup tmux

--- home/user/.config/fish/config.fish
+++ home/user/.config/fish/config.fish
@@ -1,15 +1,16 @@
 if status is-interactive
-    # Do Nothing
+  if not test -n "$TMUX"
+    tmux new
+  else
+    tmux attach
+  end
 end
 
 if not contains "/home/$USER/.local/bin" $PATH
-	set PATH $PATH /home/malwareslayer/.local/bin
+  set PATH $PATH /home/malwareslayer/.local/bin
 end
 
 set GPG_TTY (tty)
 
 gpg-connect-agent updatestartuptty /bye > /dev/null
 
-if not test -n "$TMUX"
-    tmux
-end
