09717931d1d4802e6c3e76d66437f77f2fde4ca8
Dwi Mulia Mokoginta <dwi-mulia-mokoginta@protonmail.com>
2025-01-13 22:57:33 +0700

    Fix changed base directory

--- home/user/.config/fish/functions/fish_zerotier.fish
+++ home/user/.config/fish/functions/fish_zerotier.fish
@@ -1,3 +1,3 @@
 function fish_zerotier -d 'Show Interface & Address Default Node'
-    zerotier-cli -D/home/$USER/.local/share/zerotier-one listnetworks | awk '/node-zero/ {print $8, $9}'
+    zerotier-cli -D/home/$USER/.local/lib/zerotier-one listnetworks | awk '/node-zero/ {print $8, $9}'
 end
