f2b73adbca3ae12379924a6c596445f43d9a624f
Dwi Mulia Mokoginta <dwi-mulia-mokoginta@protonmail.com>
2025-01-24 17:27:49 +0700

    Since using moving lib in local to state that are proper

--- home/user/.config/fish/functions/fish_zerotier.fish
+++ home/user/.config/fish/functions/fish_zerotier.fish
@@ -1,3 +1,3 @@
 function fish_zerotier -d 'Show Interface & Address Default Node'
-    zerotier-cli -D/home/$USER/.local/lib/zerotier-one listnetworks | awk '/node-zero/ {print $8, $9}'
+    zerotier-cli -D/home/$USER/.local/state/zerotier-one listnetworks | awk '/node-zero/ {print $8, $9}'
 end
